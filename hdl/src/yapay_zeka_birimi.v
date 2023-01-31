fd8e698b4555a8941ad19d8121b5c3b3f29dd134
kirbyydoge <aqwoguz@gmail.com>
2023-01-31 15:39:13 +0300

    Yapay zeka birimine toplama ve cikarma algoritmalarini ekler.

--- hdl/src/yapay_zeka_birimi.v
+++ hdl/src/yapay_zeka_birimi.v
@@ -37,7 +37,26 @@ reg [`CNN_YAZMAC_BIT-1:0]   sayac_w_ns;
 reg [`CNN_YAZMAC_BIT-1:0]   sayac_sonuc_r;
 reg [`CNN_YAZMAC_BIT-1:0]   sayac_sonuc_ns;
 
-wire [`CNN_YAZMAC_BIT-1:0]   sayac_min_w;
+reg [`CNN_YAZMAC_BIT-1:0]   sayac_istek_r;
+reg [`CNN_YAZMAC_BIT-1:0]   sayac_istek_ns;
+
+wire [`CNN_YAZMAC_BIT-1:0]  sayac_min_w;
+
+localparam                  DURUM_BOSTA = 'd0;
+localparam                  DURUM_ISTEK = 'd1;
+
+reg                         spekulatif_durum_r;
+reg                         spekulatif_durum_ns;
+
+reg  [`VERI_BIT-1:0]        toplayici_is0_cmb;
+reg  [`VERI_BIT-1:0]        toplayici_is1_cmb;
+wire [`VERI_BIT-1:0]        toplayici_sonuc_w;
+
+reg  [`VERI_BIT-1:0]        carpici_is0_cmb;
+reg  [`VERI_BIT-1:0]        carpici_is1_cmb;
+reg                         carpici_gecerli_cmb;
+wire [2*`VERI_BIT-1:0]      carpici_sonuc_w;
+wire                        carpici_sonuc_gecerli_w;
 
 always @* begin
     for (i = 0; i < `N_CNN_YAZMAC; i = i + 1) begin
@@ -47,8 +66,14 @@ always @* begin
     sayac_x_ns = sayac_x_r;
     sayac_w_ns = sayac_w_r;
     sayac_sonuc_ns = sayac_sonuc_r;
+    sayac_istek_ns = sayac_istek_r;
     islem_sonuc_ns = islem_sonuc_r;
     islem_gecerli_cmb = `HIGH;
+    toplayici_is0_cmb = islem_sonuc_r;
+    toplayici_is1_cmb = carpici_sonuc_w[0 +: `VERI_BIT];
+    carpici_is0_cmb = bellek_x_r[sayac_istek_r];
+    carpici_is1_cmb = bellek_w_r[sayac_istek_r];
+    carpici_gecerli_cmb = `LOW;
 
     case(islem_kod_i)
     `UOP_YZB_LDX_OP1: begin
@@ -72,11 +97,13 @@ always @* begin
     `UOP_YZB_CLRX: begin
         sayac_x_ns = {`CNN_YAZMAC_BIT{1'b0}};
         sayac_sonuc_ns = {`CNN_YAZMAC_BIT{1'b0}}; // Spekulatif carpimlari geri sar
+        sayac_istek_ns = {`CNN_YAZMAC_BIT{1'b0}};
         islem_sonuc_ns = {`VERI_BIT{1'b0}};
     end
     `UOP_YZB_CLRW: begin
         sayac_w_ns = {`CNN_YAZMAC_BIT{1'b0}};
         sayac_sonuc_ns = {`CNN_YAZMAC_BIT{1'b0}}; // Spekulatif carpimlari geri sar
+        sayac_istek_ns = {`CNN_YAZMAC_BIT{1'b0}};
         islem_sonuc_ns = {`VERI_BIT{1'b0}};
     end
     `UOP_YZB_RUN: begin
@@ -84,10 +111,27 @@ always @* begin
     end
     endcase
 
-    if (sayac_sonuc_r < sayac_min_w) begin
-        islem_sonuc_ns = bellek_x_r[sayac_sonuc_r] * bellek_w_r[sayac_sonuc_r] + islem_sonuc_r;
-        sayac_sonuc_ns = sayac_sonuc_r + 1;
+    case(spekulatif_durum_r)
+    DURUM_BOSTA: begin
+        if (sayac_sonuc_r < sayac_min_w) begin
+            carpici_gecerli_cmb = `HIGH;
+            spekulatif_durum_ns = DURUM_ISTEK;
+        end
     end
+    DURUM_ISTEK: begin
+        if (carpici_sonuc_gecerli_w) begin
+            islem_sonuc_ns = toplayici_sonuc_w;
+            sayac_sonuc_ns = sayac_sonuc_r + 1;
+            if (sayac_istek_r < sayac_min_w) begin
+                carpici_gecerli_cmb = `HIGH;
+                spekulatif_durum_ns = DURUM_ISTEK;
+            end
+            else begin
+                spekulatif_durum_ns = DURUM_BOSTA;
+            end
+        end
+    end
+    endcase
 end
 
 always @(posedge clk_i) begin
@@ -99,7 +143,9 @@ always @(posedge clk_i) begin
         sayac_x_r <= {`CNN_YAZMAC_BIT{1'b0}};
         sayac_w_r <= {`CNN_YAZMAC_BIT{1'b0}};
         sayac_sonuc_r <= {`CNN_YAZMAC_BIT{1'b0}};
+        sayac_istek_r <= {`CNN_YAZMAC_BIT{1'b0}};
         islem_sonuc_r <= {`VERI_BIT{1'b0}};
+        spekulatif_durum_r <= DURUM_BOSTA;
     end
     else begin
         for (i = 0; i < `N_CNN_YAZMAC; i = i + 1) begin
@@ -109,9 +155,27 @@ always @(posedge clk_i) begin
         sayac_x_r <= sayac_x_ns;
         sayac_w_r <= sayac_w_ns;
         sayac_sonuc_r <= sayac_sonuc_ns;
+        sayac_istek_r <= sayac_istek_ns;
         islem_sonuc_r <= islem_sonuc_ns;
+        spekulatif_durum_r <= spekulatif_durum_ns;
     end
 end
+
+carpici carp (
+    .clk_i            ( clk_i ),
+    .islec0_i         ( carpici_is0_cmb ),
+    .islec1_i         ( carpici_is1_cmb ),
+    .islem_gecerli_i  ( carpici_gecerli_cmb ),
+    .carpim_o         ( carpici_sonuc_w ),
+    .carpim_gecerli_o ( carpici_sonuc_gecerli_w )
+);
+
+toplayici topla (
+    .islec0_i ( toplayici_is0_cmb ),
+    .islec1_i ( toplayici_is1_cmb ),
+    .carry_i  ( 1'b0 ),
+    .toplam_o ( toplayici_sonuc_w )
+);
 
 assign sayac_min_w = sayac_x_r < sayac_w_r ? sayac_x_r : sayac_w_r;
 assign islem_sonuc_o = islem_sonuc_r;
